46d176088ec2b5bba24be89e3487da5928a84e08
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2025-07-10 21:22:05 +0300

    Add files via upload

---
+++ utils/send-email.ts
@@ -0,0 +1,23 @@
+import { FormData } from "@/app/ui/ContactsForm/ContactsForm";
+import { Resend } from 'resend';
+
+
+
+
+export async function sendEmail(userData: FormData) {
+    
+  // const { data, error } = await resend.emails.send({
+  //   from: userData.companyEmail,
+  //   to: ['angel04172002@gmail.com'],
+  //   subject: '',
+  //   html: `<p>${userData.otherInfo}</p>`
+  // });
+
+
+  // if (error) {
+  //   return Response.json(error);
+  // }
+
+  // return Response.json({ message: 'Имейлът е изпратен успешно!' });
+
+}

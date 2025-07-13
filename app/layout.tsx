128cb2f7a08f7eb4bc2bfa4e89fb544531fcf8bf
Angel04172002 <angel04172002@gmail.com>
2025-07-13 22:07:14 +0300

    Small changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 
     <html lang="en">
 
-
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
 
 

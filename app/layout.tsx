5204eb5520d413b7abcf4b29965c3d4ebebdaf94
Angel04172002 <angel04172002@gmail.com>
2025-07-13 18:00:08 +0300

    Change meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
 
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, maximum-scale=1.0,  user-scalable=0" />
       </head>
 
       <body>
